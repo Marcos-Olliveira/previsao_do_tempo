5df9f590aeb9ae5896ff365c0a3cff8d7586854a
Marcos Roberto <freelancer.devmarcos@gmail.com>
2026-05-28 07:08:11 -0300

    feature/funcionalidade do botao buscar adicionada

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const tabela_previsao = document.querySelector(".main-container")
 
+// Função responsável por alternar manualmente a visibilidade do container
+// de resultados, mostrando a tabela quando necessário e ocultando-a quando
+// a busca não estiver ativa.
+function remover_hidden() {
+    if (tabela_previsao.hidden === true) {
+        tabela_previsao.hidden = false
+    } else {
+        tabela_previsao.hidden = true
+     }
+}
+
+// Função vinculada ao botão de busca que exibe a tabela de previsão de tempo
+// para a cidade pesquisada, alternando a visibilidade do container de resultados.
+function buscar_previsao() {
+    remover_hidden()
+}
