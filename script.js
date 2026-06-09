8ff4fbf7cb394aacdd6d18d4327ddd988fff8ac0
Marcos Roberto <freelancer.devmarcos@gmail.com>
2026-06-08 21:17:14 -0300

    alteração no fluxo do programa

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const tabela_previsao = document.querySelector(".hidden")
 
-// Função responsável por alternar manualmente a visibilidade do container
-// de resultados, mostrando a tabela quando necessário e ocultando-a quando
-// a busca não estiver ativa.
-function remover_hidden() {
-    if (tabela_previsao.hidden === true) {
-        tabela_previsao.hidden = false
-    } else {
-        tabela_previsao.hidden = true
-     }
+
+
+
+//constantes selecionadasa do html
+let caixa_de_pesquisa_central=document.getElementById("blocoCentral")
+let header=document.getElementById("header")
+let main=document.getElementById("main")
+let footer=document.getElementById("footer")
+
+
+
+function alternar() {
+    caixa_de_pesquisa_central.hidden=true
+
+    header.hidden=false;
+    main.hidden=false;
+    footer.hidden=false;
+
+    
+
 }
 
-// Função vinculada ao botão de busca que exibe a tabela de previsão de tempo
-// para a cidade pesquisada, alternando a visibilidade do container de resultados.
-function buscar_previsao() {
-    remover_hidden()
-}
