6a0a49cadd5d6bb88f5033e9fb2d397801ef4ffe
Marcos Roberto <freelancer.devmarcos@gmail.com>
2026-06-10 14:57:45 -0300

    atualização da logica/resposta da API recebida e tratada. Incompleta ainda.

--- script.js
+++ script.js
@@ -12,37 +12,39 @@ let estadoDaPagina = "buscar"
 
 
 //quando o botão é clicado, a função faz com que o html oculte a sessao buscar e exiba a sessao resultado
-buscar_botao.addEventListener("click",() => {
+buscar_botao.addEventListener("click",async function buscar() {
     
     async function chamar_API(){
-        console.log("entrou na função")
-
-        //armazena o valor digitado  pelo usuario no campo de pesquisa na sessao busca
+    
+        //armazeno o valor digitado  pelo usuario no campo de pesquisa na sessao busca
         let cidade_pesquisada = document.getElementById("busca-search-box").value;
         let cidade_formatada = encodeURIComponent(cidade_pesquisada)
         console.log("cidade:", cidade_formatada)
+        console.log("tipo:",typeof(cidade_formatada))
 
-        //cria a rede de segurança para monitorar
+        //crio a rede de segurança para monitorar
         try {
-            //cria uma variavel para armazenar a chave da api nescessaria para receber os dados!
+            //crio uma variavel para armazenar a chave da api nescessaria para receber os dados!
             const chaveApi = "df519a56"
 
-            //montar a url com as informações. Detalhe, city_name= e Key=  são obrigatorios e padronizados pelo site da api
+            //monto a url com as informações. Detalhe, city_name= e Key=  são obrigatorios e padronizados pelo site da api
             const url = `https://corsproxy.io/?https://api.hgbrasil.com/weather?city_name=${cidade_formatada}&key=${chaveApi}`;
 
-            //dispara o pedido http e aguarda o envelope da resposta
+            //disparo o pedido http e aguarda o envelope da resposta
             const resposta = await fetch(url)
 
-            //converter a resposta bruta em um objeto manipulavel
+            //converto a resposta bruta em um objeto manipulavel
             const dados = await resposta.json()
+
+            console.log(dados)
             
-            return
+    
         }
         catch(erro){      
             console.log(erro)      
         }
     }
-    chamar_API();
+    await chamar_API();
            
         
         //condição para alternar a visualização da sessão busca e resultado
